4e6c0986590d3604d4d6643dc11a7ab08b12f13a
EngHassan8 <hassannaana99@gmail.com>
2025-01-19 12:58:41 +0300

    waa tasbiix

--- src/components/Tasbiix.jsx
+++ src/components/Tasbiix.jsx
@@ -19,14 +19,15 @@ const [counter, setCouter] = useState (0)
 
 
 
-return <div className=" text-center mt-28 border-4 border-white bg-gray-100 w-[300px] sm:ml-[500px] ml-[40px]  sm:px-10">
-    <h1 className=" text-9xl"> {counter} </h1>
-    <div className="bg-gray-500 rounded-2xl">
-    <button onClick={ handleIncrement}  className=" bg-yellow-500 text-white rounded-lg px-4 py-1 my-4 font-serif ">increates </button>
+return <div className=" text-center mt-28 ">
+   <div className="border-4 border-white bg-gray-100 w-[300px] h-[150px]  sm:ml-[500px] ml-[40px]  sm:px-10 py-10">
+    <h1 className=" text-4xl text-purple-500 "> {counter}  </h1>
+    <div className="bg-gray-500 rounded-2xl ">
+    <button onClick={ handleIncrement}  className=" bg-yellow-500 text-white rounded-lg px-4 py-1 my-4 font-serif ">Count </button>
     <button onClick={handleRest} className=" bg-yellow-500 text-white rounded-lg px-4 py-1 ml-4  font-serif">  Rest  </button>
       </div>
 
-    
+      </div>
 </div>
 
 }
